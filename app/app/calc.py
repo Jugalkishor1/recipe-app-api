d9e883d458839cd09d10a5fd7762fffe670f6246
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2025-05-06 17:25:19 +0530

    Write TDD test case

---
+++ app/app/calc.py
@@ -0,0 +1,26 @@
+"""
+Calculator functions
+
+
+Write a simple text using Django.
+a test using test driven development.
+
+Create the test first and then we're going to add the functionality that
+the tests that drive the development.
+
+So you're going to write the test for the behavior you expect to see in the code.
+
+And then that test is going to fail because you haven't written the code yet.
+
+Then you're going to go back and you're going to write the code.
+
+So the test passes.
+
+"""
+
+def add(x, y):
+    """ Add x and y and return result """
+    return x + y
+
+def substract(x, y):
+    return y - x
