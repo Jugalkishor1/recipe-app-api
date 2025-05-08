0aa8f195d89480117eff437c9acbdba817332cb4
Jugal Kishor <115132207+Jugalkishor1@users.noreply.github.com>
2025-05-08 12:33:03 +0530

    Implement user model and normalise email address

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,34 @@
+"""
+Test for models
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    def test_Create_user_with_email_successfully(self):
+        """ Test create user with email successfully """
+        email = "test@example.com"
+        password = "Test@1234"
+
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """ Test email is normalized for new users """
+        sample_emails = [
+            ["test1@EXAMPLE.com", "test1@example.com"],
+            ["Test2@Example.com", "Test2@example.com"],
+            ["TEST3@EXAMPLE.COM", "TEST3@example.com"],
+            ["test4@example.COM", "test4@example.com"]
+        ]
+
+        for email, expected in sample_emails:
+            user = get_user_model().objects.create_user(email, "Test@1234")
+            self.assertEqual(user.email, expected)
